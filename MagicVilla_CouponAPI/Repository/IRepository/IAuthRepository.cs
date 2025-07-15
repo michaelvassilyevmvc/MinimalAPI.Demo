ed00b0e0508c41aae8182c3bab7cbcc4f03920a8
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-07-15 19:46:27 +0500

    Add auth repository

---
+++ MagicVilla_CouponAPI/Repository/IRepository/IAuthRepository.cs
@@ -0,0 +1,10 @@
+﻿using MagicVilla_CouponAPI.Models.DTO;
+
+namespace MagicVilla_CouponAPI.Repository.IRepository;
+
+public interface IAuthRepository
+{
+    bool IsUniqueUser(string userName);
+    Task<LoginResponseDTO> Authenticate(LoginRequestDTO loginRequestDto);
+    Task<UserDTO> Register(RegistrationRequestDTO requestDto);
+}
