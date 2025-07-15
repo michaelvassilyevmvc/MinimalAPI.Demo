20d732a4ad88db7bbf22faf9944a37aa4458e25d
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-07-15 19:02:57 +0500

    Add: New model and DTO model for auth user

--- MagicVilla_CouponAPI/Data/ApplicationDbContext.cs
+++ MagicVilla_CouponAPI/Data/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ public class ApplicationDbContext: DbContext
     }
 
     public DbSet<Coupon> Coupons { get; set; }
+    public DbSet<LocalUser> LocalUsers { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ MagicVilla_CouponAPI/Models/DTO/UserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVilla_CouponAPI.Models.DTO;
+
+public class UserDTO
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public string Name { get; set; }
+}
