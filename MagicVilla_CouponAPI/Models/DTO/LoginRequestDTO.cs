b4a4e7d979e787564896a992b230f7966c421605
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-07-15 19:45:58 +0500

    Create model dto for login request and registration request

--- MagicVilla_CouponAPI/Models/DTO/LoginRequestDTO.cs
+++ MagicVilla_CouponAPI/Models/DTO/LoginRequestDTO.cs
@@ -1,8 +1,7 @@
 ﻿namespace MagicVilla_CouponAPI.Models.DTO;
 
-public class RegistrationRequestDto
+public class LoginRequestDTO
 {
     public string UserName { get; set; }
-    public string Name { get; set; }
     public string Password { get; set; }
 }
